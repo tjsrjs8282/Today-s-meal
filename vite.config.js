40e5b882364a8b65f7ecc0be13c813a6bc3e6592
seongeon <dbtjsrjs8282@naver.com>
2023-01-01 21:23:09 +0900

    Chore: Add eslint prettier resolve folder setting :#5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: resolve(__dirname, '/src/assets') },
+      { find: '@components', replacement: resolve(__dirname, '/src/components') },
+      { find: '@pages', replacement: resolve(__dirname, '/src/pages') },
+      { find: '@styles', replacement: resolve(__dirname, '/src/styles') },
+      { find: '@', replacement: resolve(__dirname, '/src') },
+    ],
+  },
 })
