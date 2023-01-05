48b4f6f1a8bbfb0f4f593a7c3ca916e0f66ed4ca
선건 <tjsrjs21@gmail.com>
2023-01-05 17:40:50 +0900

    Markup: add intro page /#14

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Wrapper from '@components/Wrapper'
 import introLogo from '@assets/intro-logo.png'
+import $ from './intro.module.scss'
+import Button from '@components/Button'
+
 const Intro = () => {
   return (
     <Wrapper theme="column">
-      <h1>
-        오늘 하루
-        <br />
-        칼로리 알아보자
-      </h1>
-      <p>목표를 설정하고 칼로리 계산해보아요</p>
-      <img src={introLogo} alt="로고" />
+      <div className={$.introBox}>
+        <h1>
+          오늘 하루
+          <br />
+          칼로리 알아보자
+        </h1>
+        <p>목표를 설정하고 칼로리 계산해보아요</p>
+        <img src={introLogo} alt="로고" />
+      </div>
+      <Button title="시작하기" />
     </Wrapper>
   )
 }
