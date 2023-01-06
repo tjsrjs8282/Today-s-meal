a56ccc926f95880595260edd94b88e43e44aeb2a
선건 <tjsrjs21@gmail.com>
2023-01-06 16:42:51 +0900

    Markup: Add userInfo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Intro from '@pages/Intro'
+import UserInfo from '@pages/UserInfo'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Intro />}></Route>
+          <Route path="/start" element={<UserInfo />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/UserInfo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BackBtn from '@components/BackBtn'
+
+const UserInfo = () => {
+  return (
+    <div>
+      <BackBtn />
+    </div>
+  )
+}
+
+export default UserInfo
