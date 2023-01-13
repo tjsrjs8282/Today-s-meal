1c39b25408e731db00a85d056b112598b6628d1d
seongeon <dbtjsrjs8282@naver.com>
2023-01-13 23:35:27 +0900

    config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
+})
