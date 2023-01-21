1d3695ec72496832b7b9052c201775b366df497d
Shape Lee <shape1026@gmail.com>
2023-01-21 09:16:53 +0900

    Rename: Change Route from Search to FoodSearch/#32

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from '@pages/Intro'
 import UserInfo from '@pages/UserInfo'
 import UserPurpose from '@pages/UserPurpose'
 import FoodToday from '@pages/FoodToday'
-import Search from '@pages/Search'
+import FoodSearch from '@pages/FoodSearch'
 import FoodTodayDetail from '@pages/FoodTodayDetail'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Route path="/start" element={<UserInfo />}></Route>
           <Route path="/purpose" element={<UserPurpose />}></Route>
           <Route path="/today" element={<FoodToday />}></Route>
-          <Route path="/search" element={<Search />}></Route>
+          <Route path="/search" element={<FoodSearch />}></Route>
           <Route path="/today/detail" element={<FoodTodayDetail />}></Route>
         </Routes>
       </BrowserRouter>
