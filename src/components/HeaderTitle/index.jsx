2e7f8dab7a72aae7cf0fd772cd45ee7c4f0ee024
Shape Lee <shape1026@gmail.com>
2023-01-25 12:05:12 +0900

    Feat: 실시간 날짜기능 추가/#32

--- src/components/HeaderTitle/index.jsx
+++ src/components/HeaderTitle/index.jsx
@@ -1,12 +1,39 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState, useRef, useCallback } from 'react'
 import Flex from '@components/Flex'
 
+const weeks = ['일', '월', '화', '수', '목', '금', '토']
+
 const HeaderTitle = memo(({ content }) => {
+  const [today, setToday] = useState(new Date())
+  const [month, setMonth] = useState(0)
+  const [date, setDate] = useState(0)
+  const [week, setWeek] = useState('')
+  const intervel = useRef(null)
+
+  const handleCheckToday = useCallback(() => {
+    setToday(new Date())
+    setMonth(today.getMonth() + 1)
+    setDate(today.getDate())
+    setWeek(weeks[today.getDay()])
+  }, [month, date, week])
+
+  useEffect(() => {
+    handleCheckToday()
+  }, [])
+
+  useEffect(() => {
+    console.log('useEffect')
+    intervel.current = setInterval(handleCheckToday, 1000)
+
+    return () => {
+      clearInterval(intervel.current)
+    }
+  }, [month, date, week])
+
   return (
     <Flex column marginLeft start>
       <h2>{content}</h2>
-      <p>12월 28일 수요일</p>
-      {/* 실시간 날짜 기능 넣기 */}
+      <p>{month}월 {date}일 {week}요일</p>
     </Flex>
   )
 })
