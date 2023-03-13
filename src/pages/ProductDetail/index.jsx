ec5c60c2c85aaccd6d3c96e637e176a190e8b5ef
Shape Lee <shape1026@gmail.com>
2023-03-13 12:16:24 +0900

    Markup: Add DetailImageBox & Buttom Button/#110

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import $ from './productDetail.module.scss'
+import Wrapper from '@components/Wrapper'
+import Flex from '@components/Flex'
+import Header from '@components/Header'
+import IconButton from '@components/IconButton'
+import Button from '@components/Button'
 
 export default function ProductDetail() {
   return (
-    <div>
-      
-    </div>
+    <Wrapper colorWhite>
+      <Header>
+        <Flex width between>
+          <IconButton kinds="back" />
+          <IconButton kinds="cart" />
+        </Flex>
+      </Header>
+      <div className={$.detailImage}></div>
+      <Flex width fixed>
+        <Flex marginRight width>
+          <Button content="장바구니" nonefixed noneBackground />
+        </Flex>
+        <Flex marginRigth width>
+          <Button content="구매하기" nonefixed ></Button>
+        </Flex>
+      </Flex>
+    </Wrapper>
   )
 }
