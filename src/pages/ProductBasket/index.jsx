ed885d4c3ff8b5c14a055c5fd74651410312c534
Shape Lee <shape1026@gmail.com>
2023-03-16 16:20:08 +0900

    Markup: Add top header component action/#128

--- src/pages/ProductBasket/index.jsx
+++ src/pages/ProductBasket/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Wrapper from '@components/Wrapper'
+import Flex from '@components/Flex'
+import Header from '@components/Header'
+import HeaderTitle from '@components/HeaderTitle'
+import IconButton from '@components/IconButton'
 
 export default function ProductBasket() {
   return (
-    <div>
-      
-    </div>
+    <Wrapper colorGray>
+      <Header>
+        <Flex width between>
+          <HeaderTitle content="장바구니" />
+          <IconButton kinds="close" />
+        </Flex>
+      </Header>
+    </Wrapper>
   );
 };
