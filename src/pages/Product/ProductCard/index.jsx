2366fecd13c3321dd351bba43e47d6910bfba739
seongeon <dbtjsrjs8282@naver.com>
2023-03-17 17:51:07 +0900

    Feat: Add mypage intakeModal function

--- src/pages/Product/ProductCard/index.jsx
+++ src/pages/Product/ProductCard/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Flex from '@components/Flex'
+import { Link } from 'react-router-dom'
 
 export default function ProductCard({ data }) {
+  console.log(data.id)
   return (
     <Flex column radius marginBottom padding shadow col3 start colorWhite fontWhite>
-      <Flex image radius marginBottom>
-        <img src={data?.img} alt={data?.title} />
-      </Flex>
-      <h2>{data?.title}</h2>
-      <p>
-        {data?.price}
-        <span>원</span>
-      </p>
-      <p>{data?.new ? '신제품' : null}</p>
+      <Link to={`${data.id}`}>
+        <Flex image radius marginBottom>
+          <img src={data?.img} alt={data?.title} />
+        </Flex>
+        <h2>{data?.title}</h2>
+        <p>
+          {data?.price}
+          <span>원</span>
+        </p>
+        <p>{data?.new ? '신제품' : null}</p>
+      </Link>
     </Flex>
   )
 }
