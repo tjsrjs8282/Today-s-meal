3fcc8489991e9bf9161d93c23acf08b9b156dea6
seongeon <dbtjsrjs8282@naver.com>
2023-03-25 00:37:17 +0900

    Feat : Add shopping cart button function

--- src/pages/ProductBasket/index.jsx
+++ src/pages/ProductBasket/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import $ from './productBasket.module.scss'
 import Wrapper from '@components/Wrapper'
@@ -9,61 +9,48 @@ import IconButton from '@components/IconButton'
 import Button from '@components/Button'
 import CountBox from '@components/CountBox'
 import CheckBox from './CheckBox'
-import { PRODUCT_LIST } from '@pages/Product/productData.js'
+import Modal from '@components/Modal'
 import classNames from 'classnames/bind'
 import BgImg from '@assets/ic-logo-bg.png'
-
+import { localStorageService } from '@utils/localStorage.service'
 const cx = classNames.bind($)
-
-const list = [
-  {
-    id: PRODUCT_LIST[0].id,
-    title: PRODUCT_LIST[0].title,
-    price: PRODUCT_LIST[0].price,
-    img: PRODUCT_LIST[0].img,
-  },
-  {
-    id: PRODUCT_LIST[1].id,
-    title: PRODUCT_LIST[1].title,
-    price: PRODUCT_LIST[1].price,
-    img: PRODUCT_LIST[1].img,
-  },
-  {
-    id: PRODUCT_LIST[2].id,
-    title: PRODUCT_LIST[2].title,
-    price: PRODUCT_LIST[2].price,
-    img: PRODUCT_LIST[2].img,
-  },
-]
-
-const price1 = 10000
-const contants = [
-  {
-    id: 1,
-    title: '총 상품 금액',
-    price: price1,
-  },
-  {
-    id: 2,
-    title: '배송비',
-    price: 3000,
-  },
-  {
-    id: 3,
-    title: '총 결제 금액',
-    price: price1 + 3000,
-    total: true,
-  },
-]
 
 export default function ProductBasket() {
   const [checkedList, setCheckedList] = useState([])
-  const [listData, setListData] = useState(list)
+  const [cartList, setCartList] = useState([])
+  const [cartDelete, setCartDelete] = useState([])
+  const [modal, setModal] = useState(false)
+  const [modalTitle, setModalTitle] = useState('')
+  const [modalContent, setModalContent] = useState('')
+  const priceTotal =
+    cartList.length !== 0 ? cartList.map((data) => data.price).reduce((acc, cur) => acc + cur) : 0
+  const deliPrice = priceTotal > 50000 || cartList.length === 0 ? 0 : 3000
+
+  const TOTAL_PRICE = [
+    {
+      id: 1,
+      title: '총 상품 금액',
+      price: priceTotal,
+    },
+    {
+      id: 2,
+      title: '배송비',
+      price: deliPrice,
+    },
+    {
+      id: 3,
+      title: '총 결제 금액',
+      price: priceTotal + deliPrice,
+      total: true,
+    },
+  ]
+
   const navigate = useNavigate()
+
   const goBack = () => {
     navigate(-1)
   }
-  
+
   const handleCheckedItem = (id, isChecked) => {
     if (isChecked) {
       setCheckedList((prev) => [...prev, id])
@@ -76,38 +63,73 @@ export default function ProductBasket() {
   const handleAllChecked = (e) => {
     if (e.target.checked) {
       const allCheckedList = []
-      listData.forEach((item) => allCheckedList.push(item.id.toString()))
+      cartList.forEach((item) => allCheckedList.push(item.id.toString()))
 
       setCheckedList(allCheckedList)
     } else {
       setCheckedList([])
     }
-    console.log('handleAllChecked', checkedList)
+    console.log('handleAllChekecd', checkedList)
   }
 
-  const handleClickDelete = (e) => {
-    const checkedId = e.currentTarget.parentNode.parentNode.parentNode.id
-    setListData(
-      listData.filter((item) => {
-        return item.id.toString() !== checkedId
-      })
-    )
+  const onClickDeleteHandler = async (e) => {
+    const checkedId = await e.currentTarget.parentNode.parentNode.parentNode.id
+    const cartCheckFilter = await cartList.filter((item) => {
+      return item.id.toString() === checkedId
+    })
+    const cartDeleteFilter = await cartList.filter((item) => {
+      return item.id.toString() !== checkedId
+    })
+    setCartDelete(cartDeleteFilter)
+    setModalTitle(`삭제하기`)
+    setModalContent(`${cartCheckFilter[0].title} 를(을)
+     삭제 하시겠습니까?`)
+    setModal(!modal)
   }
 
-  const handleCheckedDelete = () => {
-    const listCopy = listData.filter((item) => {
+  const handleClickDelete = () => {
+    setCartList(cartDelete)
+    localStorageService().set('CART', cartDelete)
+    setModal(false)
+  }
+
+  const handleCheckedDelete = async () => {
+    const listCopy = await cartList.filter((item) => {
       return !checkedList.includes(item.id.toString())
     })
-    setListData(listCopy)
-    console.log(listCopy)
+    setCartList(listCopy)
+    localStorageService().set('CART', listCopy)
   }
+
+  const modalOnClose = () => {
+    setModal(false)
+  }
+
+  function getCartList() {
+    const sessionCart = localStorageService().get('CART')
+    const sessionCheck = sessionCart ? sessionCart : []
+    setCartList(sessionCheck)
+  }
+
+  useEffect(() => {
+    getCartList()
+  }, [])
 
   return (
     <Wrapper colorGray>
+      {modal && (
+        <Modal
+          title={modalTitle}
+          content={modalContent}
+          onClick={handleClickDelete}
+          onClose={modalOnClose}
+          confirm
+        ></Modal>
+      )}
       <Header>
         <Flex width between>
           <HeaderTitle content="장바구니" />
-          <IconButton kinds="close" onClick={goBack}/>
+          <IconButton kinds="close" onClick={goBack} />
         </Flex>
       </Header>
       <div>
@@ -119,7 +141,7 @@ export default function ProductBasket() {
               checked={
                 checkedList.length === 0
                   ? false
-                  : checkedList.length === listData.length
+                  : checkedList.length === cartList.length
                   ? true
                   : false
               }
@@ -128,7 +150,7 @@ export default function ProductBasket() {
               kinds={
                 checkedList.length === 0
                   ? 'checkNone'
-                  : checkedList.length === listData.length
+                  : checkedList.length === cartList.length
                   ? 'check'
                   : 'checkNone'
               }
@@ -138,13 +160,13 @@ export default function ProductBasket() {
           <Button content="선택삭제" onClick={handleCheckedDelete} border />
         </Flex>
         <ul className={$.list_container}>
-          {listData.length === 0 ? (
+          {cartList.length === 0 ? (
             // 리스트 없을 때
             <li className={$.empty_list}>
               <img src={BgImg} alt="빈 접시 이미지" />
             </li>
           ) : (
-            listData.map((li) => {
+            cartList.map((li) => {
               const { id, title, price, img } = li
               return (
                 <li key={id} id={id}>
@@ -168,7 +190,7 @@ export default function ProductBasket() {
                       <p className={$.price}>{price}원</p>
                     </Flex>
                     <div className={$.close_button}>
-                      <IconButton kinds="close" onClick={handleClickDelete} />
+                      <IconButton kinds="close" onClick={onClickDeleteHandler} />
                     </div>
                   </div>
                 </li>
@@ -178,7 +200,7 @@ export default function ProductBasket() {
         </ul>
       </div>
       <ul className={$.order_container}>
-        {contants.map((li) => {
+        {TOTAL_PRICE.map((li) => {
           const { id, title, price, total } = li
           return (
             <li key={id}>
